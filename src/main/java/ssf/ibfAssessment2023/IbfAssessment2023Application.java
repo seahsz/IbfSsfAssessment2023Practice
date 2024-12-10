e58aa790fba2c1fba422bb68d8c23acc8f8b417c
Seah Shun Zhou <seahsz@gmail.com>
2024-12-11 01:20:25 +0800

    Initial Commit. 30 Min spent

---
+++ src/main/java/ssf/ibfAssessment2023/IbfAssessment2023Application.java
@@ -0,0 +1,13 @@
+package ssf.ibfAssessment2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbfAssessment2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbfAssessment2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/ssf/ibfAssessment2023/IbfAssessment2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.ibfAssessment2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbfAssessment2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
